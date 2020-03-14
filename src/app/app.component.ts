86e690e0ea5d1cf04dfa3af02d3a44fdffb9dd50
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-03-14 17:02:16 -0400

    Started Hero Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'AngularServer';
+  title = 'Employee Reimbursment System';
 }
