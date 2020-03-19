327322d7e982dc50693004e30233f8e7e036da17
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-03-19 12:52:43 -0400

    sorted modules together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/components/heroes/heroes.component';
-import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './heroes/components/hero-detail/hero-detail.component';
 import { MessagesComponent } from './ers/components/messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
